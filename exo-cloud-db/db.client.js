efa6d7d4a3e6655f9c7e7ffd8adcc2646c9b7f2c
yass <yassine.benzenine49@hotmail.fr>
2025-03-04 10:52:04 +0100

    db update

--- exo-cloud-db/db.client.js
+++ exo-cloud-db/db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  'postgresql://esgi_cloud_exam_2_43864_db_user:42tsWnjHfTog8GTvte9SiZhffIwI8noP@dpg-cv3coil2ng1s73fuaro0-a/esgi_cloud_exam_2_43864_db', // TODO
   {
     dialectOptions: {
       ssl: {
